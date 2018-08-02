cd2d7d1c19b32fa0df7e1c5b24ec5bae0588d769
AlfredoArandan <alfredo.arandan@gmail.com>
2018-08-01 23:37:14 -0400

    Carga de direcotrios y archivos

---
+++ Code/precio_casas_code.R
@@ -0,0 +1,4 @@
+### Tareas:
+# 1) Estudiar la significancia estadística de las variables para la predicción del precio de las casas (hint: análisis ANOVA)
+# 2) Según resultado anterior, predecir el precio de las casas usando al menos 3 modelos de predicción (usando cross-validation). 
+# 3) Medir la capacidad predictiva del test set usando MAPE, RMS y MAE
