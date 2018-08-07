05b9eb23131d5b094c4cdc5b65f7b804782fd36e
PaulinaJC <paulina.jaureguica@gmail.com>
2018-08-07 17:18:26 -0400

    NO ME SALE!

--- Code/tarea aneurisma.R
+++ Code/tarea aneurisma.R
@@ -1,20 +1,64 @@
 library(readr)
-dataset_aneurisma <- read_delim("practice_machine_learning/Data/dataset_aneurisma.csv", 
+dataset_aneurisma <- read_delim("Data/dataset_aneurisma.csv", 
                                 ";", escape_double = FALSE, trim_ws = TRUE)
  aneurisma <- dataset_aneurisma
+ 
  # Los valores no numericos hay que trabajarlos como factors
  aneurisma$estado <- as.factor(aneurisma$estado)
  aneurisma$genero <- as.factor(aneurisma$genero)
  
-  # estadisticas clasicas de cada variables: min, max, media, median y quatiles
+ # Estadisticas clasicas de cada variables: min, max, media, median y quatiles
  summary(aneurisma)
  names(aneurisma)
  
+ # veo que clase de dato es, si es numerico, factor, etc.
+ str(aneurisma) 
+ 
  # Anova analysis. La función utilizada es aov(), la estructura es aov(variable a predecir ~ parametros para modelar, data)
  # Tarea: construir variable "estado_bin" que signfica 1 cuando es NOROTO y 0 cuando es ROTO
- res.aov <- aov(estado_bin ~  N_womersley +  wss_mean + wss_max +  osi + rrt + area_aneurisma_mm2 + vol_aneurisma_mm3 + cuello_mm + ancho_mm + alto_mm + ar + nsi + bf + angle +  mh + vol_geo_mm3 + sup_total_mm2 + strain_von_mises_real + deformation + vol_aneurisma + strain_von_mises , data = aneurisma)
+ res.aov <- aov(estado_bin ~  N_womersley +  wss_mean + wss_max +  osi + rrt + area_aneurisma_mm2 + 
+                  vol_aneurisma_mm3 + cuello_mm + ancho_mm + alto_mm + ar + nsi + bf + angle +  mh + 
+                  vol_geo_mm3 + sup_total_mm2 + strain_von_mises_real + deformation + vol_aneurisma + 
+                  strain_von_mises , data = aneurisma)
  
- # mostrar resultado del analisis ANOVA
+ # Mostrar resultado del analisis ANOVA
  summary(res.aov)  
- str(aneurisma) # veo que clase de dato es, si es numerico, factor, etc.
  
+
+  # Parte II
+ 
+ library(caret)
+ 
+ #Creación de partición de los datos para entrenamiento
+ 
+ set.seed(1)
+ intrain <- createDataPartition(y = aneurisma$estado_bin, p= 0.8, list = FALSE)
+ training <- aneurisma[intrain,]
+ testing <- aneurisma[-intrain,]
+ 
+ 
+ #Train control para la prediccion
+ 
+ control_train <- trainControl(method = "repeatedcv", number = 10,
+                               repeats = 3,
+                               classProbs = FALSE,
+                               summaryFunction = twoClassSummary,
+                               savePredictions = TRUE)
+ 
+ #Modelo 1
+ 
+ set.seed(1)
+ modelo_rf <- train(estado_bin ~ N_womersley +  wss_mean + wss_max +  osi + rrt + area_aneurisma_mm2 + 
+                      vol_aneurisma_mm3 + cuello_mm + ancho_mm + alto_mm + ar + nsi + bf + angle +  mh + 
+                      vol_geo_mm3 + sup_total_mm2 + strain_von_mises_real + deformation + vol_aneurisma + 
+                      strain_von_mises, data = training,
+                    method = "ranger",
+                    num_trees = 300,
+                    trControl = control_train)
+ summary(modelo_rf)
+ predicciones <- predict(modelo_rf, testing)
+ error <- sqrt((sum((predicciones-testing$estado-bin)^2))/nrow(testing))
+ plot(testing$estado_bin, predicciones)
+ abline(0,1)
+ mean(aneurisma$estado_bin)
+ cor(testing$price, predicciones)
