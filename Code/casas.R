a7434844d35d2cc730916845c9937b6f5add953a
PaulinaJC <paulina.jaureguica@gmail.com>
2018-08-06 23:18:30 -0400

    Modelo lm y glm

--- Code/casas.R
+++ Code/casas.R
@@ -4,37 +4,43 @@ kc_house_data <- read_csv("Data/kc_house_data.csv")
 casas <- kc_house_data
 
 
-# estadisticas clasicas de cada variables: min, max, media, median y quatiles
+# Estadisticas clasicas de cada variables: min, max, media, median y quatiles
 summary(casas)
+
+#Nombre de las variables
 names(casas)
-str(casas) # veo que clase de dato es, si es numerico, factor, etc.
 
+#Clase de dato, si es numerico, factor, etc
+str(casas) 
 
+#Parte I
 # Anova analysis. La función utilizada es aov(), la estructura es aov(variable a predecir ~ parametros para modelar, data)
 
 res.aov <- aov( price ~  bedrooms + bathrooms + sqft_living + sqft_lot + floors + waterfront + view + 
                   condition + grade + sqft_above + sqft_basement + yr_built + yr_renovated + sqft_living15
                 + sqft_lot15, data = casas)
 
-# mostrar resultado del analisis ANOVA
+# Mostrar resultado del analisis ANOVA
 summary(res.aov)  
 nrow(casas)
 head(casas)
 table(casas$floors) #conteo de cuantas casas tienen esa cantidad de pisos
 
 
-# Modelando prediccion
-
+# Parte II
 
 install.packages("caret")
-
 library(caret)
 
+#Creación de partición de los datos para entrenamiento
 
 set.seed(1)
 intrain <- createDataPartition(y = casas$price, p= 0.6, list = FALSE)
 training <- casas[intrain,]
 testing <- casas[-intrain,]
+
+
+#Train control para la prediccion
 
 control_train <- trainControl(method = "repeatedcv", number = 10,
                       repeats = 3,
@@ -45,26 +51,27 @@ control_train <- trainControl(method = "repeatedcv", number = 10,
 
 #Modelo 1
 
-set.seed(1)
+#Alternativa
 #modelo1 <- lm(price ~ bedrooms + bathrooms + sqft_living + sqft_lot + waterfront + view + condition + grade + 
-                #sqft_above + yr_built + sqft_living15 + sqft_lot15, data=training)
+#sqft_above + yr_built + sqft_living15 + sqft_lot15, data=training)
+
+set.seed(1)
 modelo_lm <- train(price ~ ., data = training,
                           method = "lm",
                           metric = "RMSE",
                           trControl = control_train)
 summary(modelo_lm)
-
 predicciones <- predict(modelo_lm, testing)
-
 error <- sqrt((sum((predicciones-testing$price)^2))/nrow(testing))
-
 plot(testing$price, predicciones)
 abline(0,1)
 mean(casas$Price)
 cor(testing$price, predicciones)
+
 #Error de predicción del modelo. Al tratarse de una variable continua se emplea como medida 
 #de error el MSE (mean square error).
 #mean((casas$price[-intrain] - predicciones)^2)
+
 
 #Modelo 2 
 
@@ -73,7 +80,7 @@ modelo_glm <- train(price ~ ., data = training,
                   method = "glm",
                   metric = "RMSE",
                   trcontrol = control_train,
-                  family = gaussian)
+                  family = "gaussian")
 
 summary(modelo_glm)
 
